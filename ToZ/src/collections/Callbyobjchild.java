be017e7f2bf5386006dcb316aff99b6134da73b4
dell46 <dell46@dell46.SMRINDIADOM.local>
2021-03-29 20:14:57 +0530

    Pushing for a querry

---
+++ ToZ/src/collections/Callbyobjchild.java
@@ -0,0 +1,38 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class Callbyobjchild {
+
+
+
+	public static void main(String[] args) {
+		
+		
+		
+		Callbyobject x1 = new  Callbyobject(3, 5);
+		Callbyobject x2 = new  Callbyobject(2, 5);
+		
+		x1.add();
+		x2.add();
+			
+						
+		/*ArrayList<Callbyobject> arr = new ArrayList<Callbyobject>();
+		arr.add(x);
+		arr.add(y);
+		
+		Iterator<Callbyobject> itr = arr.iterator();
+		while(itr.hasNext()) {
+			Callbyobject w = itr.next();
+			System.out.println(w.a);*/
+						
+		}
+	
+	
+		
+
+	}
+
+
