58b6c93639c78bbbf8e3f5b330634e7a5989a878
Sundar V <sundar@talentoz.com>
2021-03-29 20:17:31 +0530

    Parent class considered

---
+++ ToZ/src/collections/Callbyobject.java
@@ -0,0 +1,19 @@
+package collections;
+
+public class Callbyobject {
+	
+       int a;
+       int b;
+       
+       public Callbyobject(int a, int b) {
+    	   this.a = a;
+    	   this.b =b;
+       }
+
+	
+	public void add() {
+		int c= a+b;
+		System.out.println("The addition value is:\t" +c);
+	}
+
+}
